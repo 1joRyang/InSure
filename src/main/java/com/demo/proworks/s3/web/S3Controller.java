0c319f5299e793a9e30badadcad28214117fa2c0
leejihyeon114 <leejihyeon114@gmail.com>
2025-07-15 12:29:51 +0900

    feat:s3 이미지 조회 api

--- src/main/java/com/demo/proworks/s3/web/S3Controller.java
+++ src/main/java/com/demo/proworks/s3/web/S3Controller.java
@@ -14,6 +14,8 @@ import com.demo.proworks.s3.vo.S3UrlVo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.inswave.elfw.annotation.ElDescription;
 import com.inswave.elfw.annotation.ElService;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.inswave.elfw.annotation.ElValidator;
 
 @Controller
 public class S3Controller {
@@ -52,4 +54,30 @@ public class S3Controller {
     	
     	 
     }
+    
+    /**
+     * 청구이미지를 s3에서 조회합니다.
+	 * @param param objectKey에 S3 파일 키를 담아 요청
+     * @return presignedUrl에 임시 조회 URL을 담아 반환
+	 * =>DB의 INS_IMAGE_FILE테이블에서 s3_object_key 값 조회
+	 * =>s3_object_key 값을 파라미터로 /getS3ViewUrl.pwkjson API호출
+	 * =>API로 응답받은 JSON에서 presingedUrl 값 꺼내서 <img>태그의 src에 넣거나 링크로 사용하여 이미지 조회
+     */
+    @ElService(key = "getS3ViewUrl")
+    @RequestMapping(value = "getS3ViewUrl")    
+    @ElDescription(sub = "S3 Pre-signed URL 발급", desc = "S3 조회용 Pre-signed URL을 발급한다.")               
+    public S3UrlVo getS3ViewUrl(S3UrlVo param) throws Exception {    
+    
+    	 String objectKey = param.getObjectKey();
+    	 System.out.println("========조회 요청된 S3 키: " + objectKey);
+    	 
+    	 URL presignedUrl = s3Service.generatePresignedGetUrl(bucketName, objectKey);
+    	 
+    	 S3UrlVo resultVo = new S3UrlVo();
+    	 resultVo.setPresignedUrl(presignedUrl.toString());
+    	 
+    	 return resultVo;
+    }
+    
+    
 }
